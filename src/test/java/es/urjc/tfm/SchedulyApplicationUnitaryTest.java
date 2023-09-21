22b581313a01e982cebf263f3af75a6efa140377
molynx <molynx2013@gmail.com>
2023-09-22 00:07:38 +0200

    main workflow and application test

---
+++ src/test/java/es/urjc/tfm/SchedulyApplicationUnitaryTest.java
@@ -0,0 +1,14 @@
+package es.urjc.tfm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import es.urjc.tfm.scheduly.SchedulyApplication;
+
+@SpringBootTest(classes = { SchedulyApplication.class})
+public class SchedulyApplicationUnitaryTest {
+
+	@Test
+    public void appLoaded() {
+    }
+}
