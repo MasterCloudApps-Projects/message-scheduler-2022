9aa78c6fe05bd0a2e6e9d7fe473389c00d6f4102
molynx <molynx2013@gmail.com>
2023-09-26 18:33:48 +0200

    added e2e test with restAssured

---
+++ src/test/java/es/urjc/tfm/scheduly/controllers/MessageRestControllerE2ETest.java
@@ -0,0 +1,42 @@
+package es.urjc.tfm.scheduly.controllers;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+
+public class MessageRestControllerE2ETest {
+
+	@BeforeEach
+    public void setUp() {
+    	RestAssured.baseURI = "https://scheduly-molynx.cloud.okteto.net";
+    }
+	
+	@Test
+	public void createMessageAndFindByIdTest() {
+        String requestJson = "{\"messageBody\": \"Random message body\"}";
+
+        Response messageCreated = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(requestJson)
+                .when()
+                .post("/api/message/")
+                .then()
+                .contentType(ContentType.JSON)
+                .body("messageBody", equalTo("Random message body"))
+                .extract().response();
+        int messageId = messageCreated.path("id");
+	    RestAssured.given()
+        			.when()
+        			.get("/api/message/{id}", messageId)
+        			.then()
+        			.contentType(ContentType.JSON)
+        			.body("messageBody", equalTo("Random message body"));
+    }
+
+}
