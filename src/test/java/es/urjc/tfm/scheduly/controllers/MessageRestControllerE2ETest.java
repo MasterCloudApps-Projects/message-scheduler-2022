7637b2b5791aa43b9229bf56a8f53bd0d10af484
molynx <molynx2013@gmail.com>
2023-09-30 14:47:43 +0200

    e2e test for getAll

--- src/test/java/es/urjc/tfm/scheduly/controllers/MessageRestControllerE2ETest.java
+++ src/test/java/es/urjc/tfm/scheduly/controllers/MessageRestControllerE2ETest.java
@@ -1,6 +1,7 @@
 package es.urjc.tfm.scheduly.controllers;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -45,6 +46,58 @@ public class MessageRestControllerE2ETest {
 	    RestAssured.given()
 					.when()
 					.get("/api/message/{id}", messageId)
+					.then()
+					.body(equalTo(""));
+    }
+	
+	@Test
+	public void createMessagesAndFindAllTest() {
+        String requestJson1 = "{\"messageBody\": \"Random message body 1\"}";
+
+        Response messageCreated1 = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(requestJson1)
+                .when()
+                .post("/api/message/")
+                .then()
+                .contentType(ContentType.JSON)
+                .body("messageBody", equalTo("Random message body 1"))
+                .extract().response();
+        int messageId1 = messageCreated1.path("id");
+        String requestJson2 = "{\"messageBody\": \"Random message body 2\"}";
+        Response messageCreated2 = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(requestJson2)
+                .when()
+                .post("/api/message/")
+                .then()
+                .contentType(ContentType.JSON)
+                .body("messageBody", equalTo("Random message body 2"))
+                .extract().response();
+        int messageId2 = messageCreated2.path("id");
+        
+        RestAssured.given()
+        			.when()
+        			.get("/api/message/")
+        			.then()
+        			.contentType(ContentType.JSON)
+        			.body(notNullValue());
+	    RestAssured.given()
+				    .when()
+			        .delete("/api/message/{id}", messageId1)
+			        .then();
+	    RestAssured.given()
+					.when()
+					.get("/api/message/{id}", messageId1)
+					.then()
+					.body(equalTo(""));
+	    RestAssured.given()
+				    .when()
+			        .delete("/api/message/{id}", messageId2)
+			        .then();
+		RestAssured.given()
+					.when()
+					.get("/api/message/{id}", messageId2)
 					.then()
 					.body(equalTo(""));
     }
