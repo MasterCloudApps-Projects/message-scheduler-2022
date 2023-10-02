cb3d0734e712cf5f4a455da5ad06e94034d77772
molynx <molynx2013@gmail.com>
2023-10-02 10:53:26 +0200

    refactor deleting dependencies out of the domain

--- src/main/java/es/urjc/tfm/scheduly/domain/ports/ComunicationService.java
+++ src/main/java/es/urjc/tfm/scheduly/domain/ports/ComunicationService.java
@@ -1,11 +1,7 @@
 package es.urjc.tfm.scheduly.domain.ports;
 
-import java.io.IOException;
-
-import com.slack.api.methods.SlackApiException;
-
 public interface ComunicationService {
 
-	public void sendMessage(String message) throws IOException, SlackApiException;
+	public void sendMessage(String message) throws Exception;
 	
 }
--- src/main/java/es/urjc/tfm/scheduly/domain/usecases/ComunicationUseCaseImpl.java
+++ src/main/java/es/urjc/tfm/scheduly/domain/usecases/ComunicationUseCaseImpl.java
@@ -1,9 +1,5 @@
 package es.urjc.tfm.scheduly.domain.usecases;
 
-import java.io.IOException;
-
-import com.slack.api.methods.SlackApiException;
-
 import es.urjc.tfm.scheduly.domain.ports.ComunicationService;
 import es.urjc.tfm.scheduly.domain.ports.ComunicationUseCase;
 
@@ -16,7 +12,7 @@ public class ComunicationUseCaseImpl implements ComunicationUseCase{
 	}
 
 	@Override
-	public void sendMessage(String message) throws IOException, SlackApiException {
+	public void sendMessage(String message) throws Exception{
 		this.comunicationService.sendMessage(message);
 	}
 
