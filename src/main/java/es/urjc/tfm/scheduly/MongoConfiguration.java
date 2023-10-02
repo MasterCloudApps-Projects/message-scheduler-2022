be47200de682ba84d17113b5b91d5950319deff1
molynx <molynx2013@gmail.com>
2023-10-02 22:41:12 +0200

    added codec for ZonedDateTime

---
+++ src/main/java/es/urjc/tfm/scheduly/MongoConfiguration.java
@@ -0,0 +1,41 @@
+package es.urjc.tfm.scheduly;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+
+@Configuration
+public class MongoConfiguration {
+
+    @Bean
+    public MongoCustomConversions customConversions(){
+        List<Converter<?,?>> converters = new ArrayList<>();
+        converters.add(DateToZonedDateTimeConverter.INSTANCE);
+        converters.add( ZonedDateTimeToDateConverter.INSTANCE);
+        return new MongoCustomConversions(converters);
+    }
+
+    enum DateToZonedDateTimeConverter implements Converter<Date, ZonedDateTime> {
+        INSTANCE;
+        @Override
+        public ZonedDateTime convert(Date source) {
+            return ZonedDateTime.ofInstant(source.toInstant(),ZoneId.of("Europe/Madrid") );
+        }
+    }
+
+    enum ZonedDateTimeToDateConverter implements Converter<ZonedDateTime, Date> {
+        INSTANCE;
+        @Override
+        public Date convert(ZonedDateTime source) {
+            return Date.from(source.toInstant());
+        }
+    }
+
+}
