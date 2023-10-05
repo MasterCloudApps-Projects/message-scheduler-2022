2cd7617b22eaeb7f17510664edd428b4e906cb03
molynx <molynx2013@gmail.com>
2023-10-05 20:13:18 +0200

    added missing dependencies and test fix

--- src/test/java/es/urjc/tfm/scheduly/controllers/MessageWebControllerE2ETest.java
+++ src/test/java/es/urjc/tfm/scheduly/controllers/MessageWebControllerE2ETest.java
@@ -2,7 +2,7 @@ package es.urjc.tfm.scheduly.controllers;
 
 import es.urjc.tfm.scheduly.SchedulyApplication;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -11,7 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
@@ -27,12 +28,18 @@ public class MessageWebControllerE2ETest {
 
 	    @BeforeAll
 	    static void setupAll() {
-	        WebDriverManager.firefoxdriver().setup();
+	        WebDriverManager.chromedriver().setup();
+
 	    }
 
 	    @BeforeEach
 	    void setup() {
-	        driver = new FirefoxDriver();
+	        ChromeOptions options = new ChromeOptions();
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--headless");
+		options.addArguments("--remote-allow-origins=*");
+		driver = new ChromeDriver(options);
 	    }
 
 	    @AfterEach
@@ -44,7 +51,7 @@ public class MessageWebControllerE2ETest {
     public void scheduleMessageAndGetAllMessageTest() {
 		driver.get("https://scheduly-molynx.cloud.okteto.net/");
 		driver.findElement(By.id("scheduleButton")).click();
-        driver.findElement(By.id("messageBody")).sendKeys("texto de prueba");
+        driver.findElement(By.id("messageBody")).sendKeys("texto de prueba nuevas");
         driver.findElement(By.id("day")).sendKeys("9");
         driver.findElement(By.id("month")).sendKeys("9");
         driver.findElement(By.id("year")).sendKeys("2099");
@@ -52,7 +59,11 @@ public class MessageWebControllerE2ETest {
         driver.findElement(By.id("minute")).sendKeys("19");
         driver.findElement(By.id("schedule")).click();
         driver.findElement(By.id("back")).click();
-        WebElement scheduledMessage = driver.findElement(By.id("elem1"));
-    	assertNotNull(scheduledMessage);
+        WebElement div = driver.findElement(By.tagName("body"));
+        java.util.List<WebElement> elements = div.findElements(By.tagName("a"));
+        WebElement lastElem = elements.get(elements.size() - 2);
+
+        assertEquals("texto de prueba nuevas", lastElem.getText().split(",")[0]);
+        
     }
 }
