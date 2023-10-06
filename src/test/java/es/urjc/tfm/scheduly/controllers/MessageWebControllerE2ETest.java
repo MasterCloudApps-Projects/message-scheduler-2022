73972551a6f99da5f22dffdd730ee5886a23cc61
molynx <molynx2013@gmail.com>
2023-10-06 17:19:56 +0200

    MessageWebE2ETest updated

--- src/test/java/es/urjc/tfm/scheduly/controllers/MessageWebControllerE2ETest.java
+++ src/test/java/es/urjc/tfm/scheduly/controllers/MessageWebControllerE2ETest.java
@@ -1,6 +1,7 @@
 package es.urjc.tfm.scheduly.controllers;
 
 import es.urjc.tfm.scheduly.SchedulyApplication;
+import es.urjc.tfm.scheduly.services.MessageService;
 
 import static java.lang.String.format;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,6 +15,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
@@ -54,25 +56,51 @@ public class MessageWebControllerE2ETest {
 		
 		// index view
 		driver.findElement(By.id("scheduleButton")).click();
-		
+		String messageBody ="selenium test text";
 		// schedule message view
-        driver.findElement(By.id("messageBody")).sendKeys("selenium test text");
-        driver.findElement(By.id("day")).sendKeys("9");
-        driver.findElement(By.id("month")).sendKeys("9");
-        driver.findElement(By.id("year")).sendKeys("2099");
-        driver.findElement(By.id("hour")).sendKeys("9");
-        driver.findElement(By.id("minute")).sendKeys("19");
-        driver.findElement(By.id("schedule")).click();
-        
+		goToScheduleMessageViewFillsAndClickSchedule(messageBody,"9","9","2055","9","19");
         // scheduled message view
         driver.findElement(By.id("back")).click();
-        WebElement div = driver.findElement(By.className("message-info"));
-        java.util.List<WebElement> elements = div.findElements(By.tagName("div"));
-        WebElement lastElem = elements.get(elements.size() - 2);
+        // index
+        WebElement lastElem = getRecentlyScheduledMessage();
+        String lastElemText = lastElem.getText().split(",")[0];
+        // verifies if the message has been scheduled
+        assertEquals(messageBody, lastElemText);
+        
+		lastElem.click();
+		// show message view
+		WebElement messageBodyCreatedMessage = driver.findElement(By.id("messageBody"));
+    	assertEquals(lastElemText, messageBodyCreatedMessage.getText());
+    	
+   }
+	
+	@Test
+    public void scheduleMessageOutOfDateTest() {
+		driver.get("https://scheduly-molynx.cloud.okteto.net/");
+		
+		// index view
+		driver.findElement(By.id("scheduleButton")).click();
+		String messageBody ="selenium test text for out of date";
+		// schedule message view
+		goToScheduleMessageViewFillsAndClickSchedule(messageBody,"9","9","1200","9","19");
+        // error view
+        driver.findElement(By.id("back")).click();
+
+		// index view
+		driver.findElement(By.id("scheduleButton")).click();
+		// schedule message view
+        goToScheduleMessageViewFillsAndClickSchedule(messageBody,"9","9","1200","9","19");
+		// error view
+		driver.findElement(By.id("scheduleButton")).click();
+		goToScheduleMessageViewFillsAndClickSchedule(messageBody,"9","9","2200","9","19");
+        // scheduled view
+		driver.findElement(By.id("back")).click();
+		// index view
+        WebElement lastElem = getRecentlyScheduledMessage();
         String lastElemText = lastElem.getText().split(",")[0];
         
 		// verifies if the message has been scheduled
-        assertEquals("selenium test text", lastElemText);
+        assertEquals(messageBody, lastElemText);
         
 		lastElem.click();
 		// show message view
@@ -80,5 +108,20 @@ public class MessageWebControllerE2ETest {
     	assertEquals(lastElemText, messageBodyCreatedMessage.getText());
     	
 	}
-	
+
+	private void goToScheduleMessageViewFillsAndClickSchedule(String messageBody, String day, String month, String year, String hour, String minute){
+        driver.findElement(By.id("messageBody")).sendKeys(messageBody);
+        driver.findElement(By.id("day")).sendKeys(day);
+        driver.findElement(By.id("month")).sendKeys(month);
+        driver.findElement(By.id("year")).sendKeys(year);
+        driver.findElement(By.id("hour")).sendKeys(hour);
+        driver.findElement(By.id("minute")).sendKeys(minute);
+        driver.findElement(By.id("schedule")).click();
+	}
+
+	private WebElement getRecentlyScheduledMessage(){
+		WebElement div = driver.findElement(By.className("message-info"));
+        java.util.List<WebElement> elements = div.findElements(By.tagName("div"));
+        return elements.get(elements.size() - 1);
+	}
 }
